00fc09320ffa84e0c71c847604eedb338578e743
Eñaut Agirre Arrizabalaga <enaut.irakasle@gmail.com>
2025-06-14 00:22:58 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,6 +27,8 @@ class DatabaseSeeder extends Seeder
             'name' => 'admin',
             'email' => 'admin@example.com',
             'password' => bcrypt('password'),
+                                 'email_verified_at' => now(),
+
         ]);
     }
 }
