1c98df31b2a4fa070c7e892ccf93fe6ae2be2f14
Eñaut AA <enaut.irakasle@gmail.com>
2025-06-24 23:45:28 +0200

    aldakea txiki bat

--- resources/views/filament/resources/casas/pages/view-casa-habitaciones.blade.php
+++ resources/views/filament/resources/casas/pages/view-casa-habitaciones.blade.php
@@ -4,13 +4,16 @@
     @else
         {{ $this->form }}
     @endif
-
-     @if (count($relationManagers = $this->getRelationManagers()))
-        <x-filament-panels::resources.relation-managers
-            :active-manager="$this->activeRelationManager"
-            :managers="$relationManagers"
-            :owner-record="$record"
-            :page-class="static::class"
-        />
-    @endif 
+<div style="font-size: 2rem; color: #1ee99b; display: flex; align-items: center; gap: 0.5rem;">
+    <x-heroicon-o-heart class="w-8 h-8" style="color: #118f7a;" />
+    Kaixo Miren
+</div>
+@if (count($relationManagers = $this->getRelationManagers()))
+    <x-filament-panels::resources.relation-managers
+        :active-manager="$this->activeRelationManager"
+        :managers="$relationManagers"
+        :owner-record="$record"
+        :page-class="static::class"
+    />
+@endif 
 </x-filament-panels::page>
